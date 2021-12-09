5d20d6c4c5edaff10ad84b5ba826b8cdedfcaa72
RicardFabien <ricard.fabienpro92@yahoo.fr>
2021-12-09 16:50:08 +0100

    Finished lines.sh

--- lines.sh
+++ lines.sh
@@ -1,9 +1,11 @@
 for file in `ls`;do
 	if [ -f $file  ];then
 		num=`wc -l $file`
+		word=`wc -w $file`
 		num=`echo $num | cut -f 1 -d " "`
+		word=`echo $word | cut -f 1 -d " "`
 		if [ $num -gt 0 ];then
-			echo "File '$file' has $num line(s)"
+			echo "File '$file' has $num line(s) and $word word(s)"
 		fi
 	fi
 done
